009b4630f584b52bb2c86afeaa98df3139ce4be0
Vitaly <vitalic.web@gmail.com>
2021-07-21 11:52:27 +0200

    Add popup component

---
+++ src/components/Popup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="add-product__btn" @click="updateProduct">Update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popup',
+  data () {
+    return {
+      inputName: 'qqqq',
+      inputPrice: 1111
+    }
+  },
+  props: {
+    update: Function
+  },
+  methods: {
+    updateProduct () {
+      this.update(this.inputName, this.inputPrice)
+    }
+  }
+}
+</script>
