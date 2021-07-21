a50cce325b3ba205d3e6f72b652effee92aa8ebd
Vitaly <vitalic.web@gmail.com>
2021-07-21 12:52:58 +0200

    Add base layout for popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <button class="add-product__btn" @click="updateProduct">Update</button>
+  <div class="popup">
+    <p class="popup__title">Enter new data</p>
+    <label class="edit-product__label">
+      Name:
+      <input
+        class="edit-product__input"
+        type="text"
+        v-model="inputName"
+        placeholder="Enter product name"
+      />
+    </label>
+    <label class="edit-product__label">
+      Price:
+      <input
+        class="edit-product__input"
+        type="text"
+        v-model="inputPrice"
+        placeholder="Enter product name"
+      />
+    </label>
+    <button
+      class="edit-product__btn"
+      type="button"
+      @click="updateProduct"
+    >Update</button>
+    <button
+      class="edit-product__close-btn"
+      type="button"
+    >x</button>
   </div>
 </template>
 
@@ -23,3 +50,53 @@ export default {
   }
 }
 </script>
+
+<style>
+.popup {
+  border: 1px solid black;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
+  position: absolute;
+  right: 181px;
+  top: 36px;
+}
+
+.popup__title {
+  margin: 0 0 5px;
+}
+
+.edit-product__label {
+  display: flex;
+  width: 176px;
+  justify-content: space-between;
+}
+
+.edit-product__label:first-of-type {
+  margin: 0 0 10px;
+}
+
+.edit-product__input {
+  width: 116px;
+}
+
+.edit-product__btn {
+  margin: 10px 0 0 0;
+}
+
+.edit-product__close-btn {
+  position: absolute;
+  right: -24px;
+  top: -27px;
+  background: none;
+  outline: none;
+  border: none;
+  font-size: 25px;
+}
+
+.edit-product__close-btn:hover {
+  cursor: pointer;
+}
+</style>
